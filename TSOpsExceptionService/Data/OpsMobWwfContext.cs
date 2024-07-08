f5f6957977a1a2c36a35c35e1b5aa48cc43aea13
Ganesh Kumar <reachoutganeshkumar@gmail.com>
2024-07-08 15:18:30 -0400

    Fixes

--- TSOpsExceptionService/Data/OpsMobWwfContext.cs
+++ TSOpsExceptionService/Data/OpsMobWwfContext.cs
@@ -1,58 +1,42 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using TSOpsExceptionService.Models;
 
-namespace TSOpsExceptionService.Data;
-
-public partial class OpsMobWwfContext : DbContext
+namespace TSOpsExceptionService.Data
 {
-    private readonly IConfiguration _configuration;
-    public OpsMobWwfContext(IConfiguration configuration)
+    public partial class OpsMobWwfContext : DbContext
     {
-        _configuration = configuration;
-    }
-
-    public OpsMobWwfContext(DbContextOptions<OpsMobWwfContext> options)
-        : base(options)
-    {
-    }
-
-    public virtual DbSet<LastWorkFlowException> LastWorkFlowExceptions { get; set; }
-
-    public virtual DbSet<ReprocessedException> ReprocessedExceptions { get; set; }
-
-    public virtual DbSet<WorkflowException> WorkflowExceptions { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        if (!optionsBuilder.IsConfigured && _configuration != null)
+        public OpsMobWwfContext(DbContextOptions<OpsMobWwfContext> options)
+            : base(options)
         {
-            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("OpsMobWwfConnectionString"));
         }
+
+        public virtual DbSet<LastWorkFlowException> LastWorkFlowExceptions { get; set; }
+        public virtual DbSet<ReprocessedException> ReprocessedExceptions { get; set; }
+        public virtual DbSet<WorkflowException> WorkflowExceptions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<LastWorkFlowException>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+            });
+
+            modelBuilder.Entity<ReprocessedException>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+            });
+
+            modelBuilder.Entity<WorkflowException>(entity =>
+            {
+                entity.HasKey(e => e.Id).HasName("PK_Requests");
+                entity.Property(e => e.Id).ValueGeneratedNever();
+                entity.Property(e => e.CreateDate).HasDefaultValueSql("(getdate())");
+                entity.Property(e => e.IsBusinessError).HasDefaultValue(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<LastWorkFlowException>(entity =>
-        {
-            entity.Property(e => e.Id).ValueGeneratedNever();
-        });
-
-        modelBuilder.Entity<ReprocessedException>(entity =>
-        {
-            entity.Property(e => e.Id).ValueGeneratedNever();
-        });
-
-        modelBuilder.Entity<WorkflowException>(entity =>
-        {
-            entity.HasKey(e => e.Id).HasName("PK_Requests");
-
-            entity.Property(e => e.Id).ValueGeneratedNever();
-            entity.Property(e => e.CreateDate).HasDefaultValueSql("(getdate())");
-            entity.Property(e => e.IsBusinessError).HasDefaultValue(false);
-        });
-
-        OnModelCreatingPartial(modelBuilder);
-    }
-
-    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 }
--- TSOpsExceptionService/Program.cs
+++ TSOpsExceptionService/Program.cs
@@ -4,12 +4,25 @@ using TSOpsExceptionService.Services;
 using TSOpsExceptionService.Data;
 using Serilog;
 using TSOpsExceptionService.Configuration.Models;
-using TSOpsExceptionService.Common;
 using TSOpsExceptionService.Mock.Services;
 using Serilog.Events;
+using Microsoft.EntityFrameworkCore;
 
 var builder = Host.CreateApplicationBuilder(args);
 
+// Set the base path to the directory where the executable is located
+builder.Configuration.SetBasePath(AppContext.BaseDirectory);
+
+// Add the appsettings.json file
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+// Read the environment name from the configuration
+var environmentName = builder.Configuration["EnvironmentSettings:Environment"];
+
+// Add the environment-specific configuration file
+builder.Configuration.AddJsonFile($"appsettings.{environmentName}.json", optional: false, reloadOnChange: true);
+
+// Configure services
 builder.Services.Configure<SoapEndpointOptions>(
     builder.Configuration.GetSection(
         key: nameof(SoapEndpointOptions)));
@@ -22,34 +35,42 @@ builder.Services.Configure<DurationOptions>(
     builder.Configuration.GetSection(
         key: nameof(DurationOptions)));
 
+// Ensure log directories exist
+var logDirectory = Path.Combine(AppContext.BaseDirectory, "Logs");
+Directory.CreateDirectory(Path.Combine(logDirectory, "Information"));
+Directory.CreateDirectory(Path.Combine(logDirectory, "Error"));
+
+
 // Configure Serilog
 Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Override("Microsoft", LogEventLevel.Warning) // Set minimum level to Warning for Microsoft logs
     .MinimumLevel.Debug()
     .Enrich.FromLogContext()
+    .WriteTo.Console() // Output logs to console
     .WriteTo.Logger(lc => lc
         .Filter.ByIncludingOnly(le => le.Level == LogEventLevel.Information)
         .WriteTo.File(
-            path: "Logs/Information/log-.txt",
+            path: Path.Combine(logDirectory, "Information", "log-.txt"),
             outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level:u3}] {Message:lj}{NewLine}{Exception}",
             rollingInterval: RollingInterval.Day,
             shared: true))
     .WriteTo.Logger(lc => lc
         .Filter.ByIncludingOnly(le => le.Level >= LogEventLevel.Error)
         .WriteTo.File(
-            path: "Logs/Error/log-.txt",
+            path: Path.Combine(logDirectory, "Error", "log-.txt"),
             outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level:u3}] {Message:lj}{NewLine}{Exception}",
             rollingInterval: RollingInterval.Day,
             shared: true))
     .CreateLogger();
 
+builder.Logging.ClearProviders();
 builder.Logging.AddSerilog();
-builder.Services.AddWindowsService();
-// Add services to the container
 
+// Add services to the container
+builder.Services.AddWindowsService();
 builder.Services.AddHostedService<Worker>();
 
-if (Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") == Constants.Development)
+if (environmentName == "Development")
 {
     builder.Services.AddSingleton<IJobServiceClient, DevJobServiceClient>();
     builder.Services.AddSingleton<IWorkflowMonitorServiceClient, DevWorkflowMonitorServiceClient>();
@@ -62,8 +83,21 @@ else
 
 // Register application services
 builder.Services.AddSingleton<IWorkFlowExceptionService, WorkFlowExceptionService>();
-builder.Services.AddSingleton<OpsMobWwfContext, OpsMobWwfContext>();
 builder.Services.AddSingleton<IDeserialization, Deserialization>();
+
+builder.Services.AddDbContext<OpsMobWwfContext>((serviceProvider, optionsBuilder) =>
+{
+    var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+    var logger = serviceProvider.GetRequiredService<ILogger<OpsMobWwfContext>>();
+    optionsBuilder.UseSqlServer(
+        configuration.GetConnectionString("OpsMobWwfConnectionString"),
+        sqlOptions => sqlOptions.EnableRetryOnFailure(
+            maxRetryCount: 10, // Number of retry attempts
+            maxRetryDelay: TimeSpan.FromSeconds(30), // Delay between retries
+            errorNumbersToAdd: null // Optional: specify additional SQL error numbers to retry on
+        ));
+    logger.LogInformation("Connection String is " + configuration.GetConnectionString("OpsMobWwfConnectionString"));
+});
 
 ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
 
